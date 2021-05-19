4610e06e31ab63ab66e856e623c49280dbceed1f
anistyadwisetiawan04 <anistyadwisetiawan04@gmail.com>
2021-05-20 00:44:23 +0700

    create example hapi

---
+++ src/notes.js
@@ -0,0 +1,10 @@
+const notes = [{
+    "id": "1",
+    "title": "Catatan A",
+    "tags": ["Android", "Web"],
+    "body": "Isi dari catatan A",
+    "createdAt": "2021-05-16T07:48:02.362Z",
+    "updatedAt":"2021-05-16T07:48:02.362Z"
+}];
+ 
+module.exports = notes;
